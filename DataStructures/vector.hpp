e8c964c9749db8c5abc4cea2fefe3c02d3d8042c
milosmikovic <losmicarina@gmail.com>
2024-05-22 15:13:44 +0200

    vector implementation

--- DataStructures/vector.hpp
+++ DataStructures/vector.hpp
@@ -19,10 +19,18 @@ class vector final
     public:
         typedef T v_type;
         
-        vector() : elements(nullptr), first_free(nullptr), cap(nullptr) {}
+        static void change_realloc_size(const size_t sz)
+        {
+            reallocate_size = sz;
+        }
+
+        vector() : elements(nullptr), first_free(nullptr), cap(nullptr) {} // default constructor
 
         vector(const vector&);            // copy constructor
-        vector& operator=(const vector&); // copy assignment        
+        vector& operator=(const vector&); // copy assignment   
+
+        vector(vector &&) noexcept; // move constructor 
+        vector& operator=(vector &&) noexcept; // move assigment    
                 
         // additional constructor
         vector(const T*, const T*);
@@ -30,6 +38,7 @@ class vector final
         ~vector(); 
 
         void push_back(const T&);  // copy the element
+        void push_back(T&&); // move the element
 
         // add elements
         size_t size() const { return first_free - elements; }
@@ -38,7 +47,6 @@ class vector final
         // iterator interface
         T* begin() const { return elements; }
         T* end() const { return first_free; }
-        
 
         T& operator[](std::size_t n) 
             { return elements[n]; }
@@ -48,6 +56,7 @@ class vector final
 
     private:
         static std::allocator<T> alloc;
+        static size_t reallocate_size;
 
         void chk_n_alloc() 
         {
@@ -56,7 +65,7 @@ class vector final
         }
 
         // used by the copy constructor, assignment operator, and destructor
-        std::pair<T*, T*> alloc_n_copy (const T*, const T*);
+        std::pair<T*, T*> alloc_n_copy(const T*, const T*);
 
         void free();             // destroy the elements and free the space
         void reallocate();       // get more space and copy the existing elements
@@ -70,6 +79,9 @@ template <typename T>
 std::allocator<T> vector<T>::alloc;
 
 template <typename T>
+size_t vector<T>::reallocate_size = 2;
+
+template <typename T>
 std::ostream& operator<<(std::ostream &os, const vector<T> &rhs)
 {
     for(T *b = rhs.begin(); b != rhs.end(); ++b)
@@ -80,13 +92,23 @@ std::ostream& operator<<(std::ostream &os, const vector<T> &rhs)
 }
 
 template <typename T>
-std::pair<T*, T*> vector<T>::alloc_n_copy (const T *begin, const T *end)
+std::pair<T*, T*> vector<T>::alloc_n_copy(const T *begin, const T *end)
 {
     T *data = alloc.allocate(end - begin);
     return std::make_pair(data, std::uninitialized_copy(begin, end, data));
 }
 
 // Constructors
+
+template <typename It>
+inline vector<It>::vector(const It *begin, const It *end)
+{
+    std::pair<It*, It*> new_data = alloc_n_copy(begin, end);
+    elements = new_data.first;
+    first_free = cap = new_data.second;
+}
+
+// cpy constructor
 template <typename T>
 inline vector<T>::vector(const vector<T> &rhs)
 {
@@ -95,16 +117,46 @@ inline vector<T>::vector(const vector<T> &rhs)
     first_free = cap = new_data.second;
 }
 
+// move constructor
+template <typename T>
+inline vector<T>::vector(vector<T> &&rhs) noexcept
+{
+    elements = rhs.elements;
+    first_free = rhs.first_free;
+    cap = rhs.cap;
+
+    rhs.elements = rhs.first_free = rhs.cap = nullptr;
+}
+
+// = ops
+
+// cpy=
 template <typename T>
 inline vector<T>& vector<T>::operator=(const vector<T> &rhs)
 {
 	// call alloc_n_copy to allocate exactly as many elements as in rhs
 	std::pair<T*, T*> data = 
 							alloc_n_copy(rhs.begin(), rhs.end());
+
 	free();
+
 	elements = data.first;
 	first_free = cap = data.second;
+
 	return *this;
+}
+
+// move=
+template <typename T>
+inline vector<T>& vector<T>::operator=(vector<T> &&rhs) noexcept
+{
+	elements = rhs.elements;
+    first_free = rhs.first_free;
+    cap = rhs.cap;
+
+    rhs.elements = rhs.first_free = rhs.cap = nullptr;
+
+    return *this;
 }
 
 template <typename T>
@@ -113,20 +165,33 @@ inline vector<T>::~vector()
     free();
 }
 
+// cpy push back
 template <typename T>
-inline void vector<T>::push_back(const T& s)
+inline void vector<T>::push_back(const T &elem)
 {
     chk_n_alloc(); // ensure that there is room for another element
 
     // construct a copy of s in the element to which first_free points
-    alloc.construct(first_free++, s);  
+    alloc.construct(first_free++, elem);  
 }
+
+// move push back
+template <typename T>
+inline void vector<T>::push_back(T &&elem)
+{
+    chk_n_alloc(); // ensure that there is room for another element
+
+    // construct a copy of s in the element to which first_free points
+    alloc.construct(first_free++, std::move(elem));  
+}
+
+
 
 template <typename T>
 inline void vector<T>::reallocate()
 {
     // we'll allocate space for twice as many elements as the current size
-    size_t new_capacity = size() ? 2 * size() : 1;
+    size_t new_capacity = size() ? reallocate_size * size() : 1;
 
 	// allocate new memory
 	T *new_data = alloc.allocate(new_capacity);
@@ -134,8 +199,19 @@ inline void vector<T>::reallocate()
 	// copy the data from the old memory to the new
 	T *dest = new_data;  // points to the next free position in the new array
     T *elem = elements; // points to the next element in the old array
+
 	for (size_t i = 0; i != size(); ++i)
-		alloc.construct(dest++, *elem++);
+    {
+        // check if move construcotr of T obj is nothrowable
+        if constexpr (std::is_nothrow_move_constructible_v<T>)
+        {
+            alloc.construct(dest++, std::move(*elem++));
+        }
+        else
+        {
+		    alloc.construct(dest++, *elem++);
+        }
+    }
 
 	free();  // free the old space once we've moved the elements
 
--- test_main.cpp
+++ test_main.cpp
@@ -1,7 +1,58 @@
 #include "DataStructures/vector.hpp"
 
+struct Foo
+{
+    friend std::ostream& operator<<(std::ostream&, const Foo&);
+
+    Foo(int a = 0, int b = 0) : a(a), b(b) {}
+    
+    Foo(const Foo &rhs) : a(rhs.a), b(rhs.b) {std::cout << "cpy=" << std::endl;}
+    Foo(Foo &&rhs) noexcept : a(rhs.a), b(rhs.b) {std::cout << "move=" << std::endl;}
+
+    Foo& operator=(const Foo &rhs)
+    {
+        std::cout << "cpy=" << std::endl;
+
+        if(this != &rhs)
+        {
+            a = rhs.a;
+            b = rhs.b;
+        }
+
+        return *this;
+    }
+
+    Foo& operator=(Foo &&rhs) noexcept
+    {
+        std::cout << "move=" << std::endl;
+
+        if(this != &rhs)
+        {
+            a = rhs.a;
+            b = rhs.b;
+
+            rhs.a = 0;
+            rhs.b = 0;
+        }
+        
+        return *this;
+    }
+
+
+    int a;
+    int b;
+};
+
+std::ostream& operator<<(std::ostream &os, const Foo &foo)
+{
+    os << foo.a << "-" << foo.b;
+    return os;
+}
+
 int main(int argc, char **argv)
 {
+
+    adstl::vector<int>::change_realloc_size(3);
 
     adstl::vector<int> vec1;
     vec1.push_back(2);
@@ -18,6 +69,31 @@ int main(int argc, char **argv)
     vec3 = vec2;
 
     std::cout << vec3 << std::endl;
+
+    adstl::vector<int> vec4(std::move(vec3));
+
+    std::cout << vec4 << std::endl;
+
+    adstl::vector<int> vec5;
+    vec5 = std::move(vec4);
+
+    std::cout << vec5 << std::endl;
+
+    std::cout << vec5[3] << std::endl;
+
+    adstl::vector<Foo> fVec1;
+    fVec1.push_back(Foo(2,3));
+    fVec1.push_back(Foo(3,4));
+    fVec1.push_back(Foo(2,3));
+    fVec1.push_back(Foo(3,4));
+    fVec1.push_back(Foo(2,3));
+    fVec1.push_back(Foo(3,4));
+    Foo f(31, 213);
+    fVec1.push_back(f);
+    std::cout << "#" << std::endl;
+    adstl::vector<Foo> fVec2(fVec1.begin(), fVec1.end());
+
+    std::cout << fVec2 << std::endl;
 
     return 0;
 }
